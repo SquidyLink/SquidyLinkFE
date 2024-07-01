cf7f91c0dcdcd6ed39bf0a07cc6aad95fe6a3cfc
Fatimah Patel <fatimahpatel10@gmail.com>
2024-07-01 16:43:07 +0100

    new page: login.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import LoginPage from "./pages/Login";
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" elements={<LoginPage/>}/>
+    </Routes>
+    </BrowserRouter>
+  
+  )
 }
 
 export default App;
