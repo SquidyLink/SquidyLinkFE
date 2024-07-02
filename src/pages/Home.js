67e47c122b02cc229e6887c8d8cbc8c102def6c2
Fatimah Patel <fatimahpatel10@gmail.com>
2024-07-02 13:31:18 +0100

    Linked facility log in and contractor log in to respective text on burger bar menu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -253,12 +253,17 @@ export default function Example() {
                   ))}
                 </div>
                 <div className="py-6">
-                  <a
-                    href="#"
+                <Link to="/facility/login" 
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Facility Log in
+                  </Link>
+                  <Link 
+                    to="/contractor/login" 
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    Log in
-                  </a>
+                    Contractor Log in
+                  </Link>
                 </div>
               </div>
             </div>
