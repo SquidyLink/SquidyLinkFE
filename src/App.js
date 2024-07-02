63db5c4a12d3659606e2b8e9f5634e03b836a2d9
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2024-07-02 13:17:36 +0100

    project page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from "./pages/Home";
 import LoginPage from "./pages/Login";
 import FacilityNew from "./pages/FacilityNew";
+import Projects from "./pages/Projects";
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
@@ -13,6 +15,7 @@ function App() {
           <Route path="/" element ={<Home/>}/>
           <Route path="/login" element ={<LoginPage/>}/>
           <Route path="/facility/new" element ={<FacilityNew/>}/>
+          <Route path="/projects" element ={<Projects/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Projects.js
@@ -0,0 +1,11 @@
+import Example from "../components/table"
+
+export default function Projects() {
+    // const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    return (
+      <div>
+        <Example />
+      </div>
+    )
+  }
